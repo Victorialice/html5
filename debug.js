2d663e33ea24ae6dda360b999eafa43b33a0ad7e
zhuwenqian <1506675573@qq.com>
2016-11-17 17:42:31 +0800

    added heart.beats.js

---
+++ debug.js
@@ -0,0 +1,7 @@
+drawLine(ctx, i * (canvas.height / lines), canvas.height / lines);
+
+drawLine(ctx, 0, 200) 
+
+ctx.fillRect(0, y, ctx.canvas.width, h); //context.fillRect(x,y,width,height);
+ctx.fillRect(0, 0, 500, 200);
+      
